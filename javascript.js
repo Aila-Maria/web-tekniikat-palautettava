07eac2f7db87dcf29f0eed2b58187d199419b490
Your Name <you@example.com>
2022-05-21 18:43:12 +0300

    javascriptin linkitys sivuille ja js koodin alotus

---
+++ javascript.js
@@ -0,0 +1,25 @@
+//tekstin vaihto
+//document.querySelector("h4").textContent=" Hanbok korean perinneasu";
+
+
+//let tietoa= document.getElementById("tietoa");
+
+//function createRow(colType,col1,col2){
+//    let tr = document.createElement("tr");
+ //   let c1 = document.createComment(colType);
+//    let c2 = document.createComment(colType);
+//    c1.textContent = col1;
+//    c2.textContent = col2;
+//    tr.append(c1,c2);
+//    return tr;
+//}
+//let table = document.createElement("table");
+//table.appendChild(createRow("th","Last name","First name"));
+//table.appendChild(createRow("td","john doe","john doe"));
+//tietoa.appendChild(table);
+
+let m = document.getElementById("infoboxi");
+m.style.color = "brown";
+
+let n=document.getElementById("tietoa");
+n.style.backgroundColor = "beige";

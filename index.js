60e15db5d0826c63d63aedaab868f1058ee7da9e
Your Name <you@example.com>
2022-05-22 14:05:17 +0300

    js tiedostojen yhteen kokoaminen

--- index.js
+++
@@ -1,5 +0,0 @@
-let m = document.getElementById("infoboxi");
-m.style.color = "brown";
-
-let n=document.getElementById("tietoa");
-n.style.backgroundColor = "beige";
--- kolmassivu.js
+++ kolmassivu.js
@@ -1,2 +1,24 @@
 //tekstin vaihto
 document.querySelector("h4").textContent=" Hanbok korean perinneasu";
+
+let m = document.getElementById("clearfix");
+m.style.color = "teal";
+
+let n=document.getElementById("clearfix");
+n.style.backgroundColor = "beige";
+
+//seuraava ja takaisin nappuloiden toiminta
+let button1= document.getElementById("home");
+let button2= document.getElementById("back");
+
+button1.addEventListener("click",alkuun );
+button2.addEventListener("click",takaisin);
+
+
+function alkuun(){
+    location.href="index.html";
+}
+
+function takaisin(){
+    location.href="toinensivu.html";
+}
